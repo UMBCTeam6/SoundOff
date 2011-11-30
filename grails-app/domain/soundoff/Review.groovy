fda1f6427abb9ee44690a6494083cb51bb9e9733
Pat Laughlin <voipme2@gmail.com>
2011-11-30 15:09:46 -0500

    Added in the constraint that ensures a user can only write a single review per album.

--- grails-app/domain/soundoff/Review.groovy
+++ grails-app/domain/soundoff/Review.groovy
@@ -9,6 +9,8 @@ class Review {
     static belongsTo = [ album: Album, creator: User ]
     
     static constraints = {
+    	// ensures that a user can only review an album once
+    	album(unique: 'creator')
         title(nullable: false, blank: false)
         text(nullable: false, blank: false)
         rating(min:1)
